85a8c93105581e058e956d7c5b638307ce1b336c
Aditya Gautam <adityascottish27@gmail.com>
2026-08-26 21:17:52 +0530

    working on the NAND2_X3 cell; error with ABC

---
+++ 26-08-26/nand_map.v
@@ -0,0 +1,5 @@
+module \$_NAND_$ (A, B, Y);
+  input A, B;
+  output Y;
+  NAND2_X3 _TECHMAP_REPLACE_ (.A(A), .B(B), .Y(Y));
+endmodule
--- 26-08-26/yosys.tcl
+++ 26-08-26/yosys.tcl
@@ -1,5 +1,29 @@
+# read_verilog full_adder.v
+
+# hierarchy -top full_adder
+# proc; opt; techmap; opt
+
+# abc -g NAND
+
+# hilomap -hicell NAND2_X3 Y -locell NAND2_X3 Y
+# abc -liberty NangateOpenCellLibrary_typical_only_NAND2_X3.lib -g NAND
+
+# write_verilog -noattr fa_nand_mapped.v
+
 read_verilog full_adder.v
-synth -top full_adder
-dfflibmap -liberty NangateOpenCellLibrary_typical_only_NAND2_X3.lib
-abc -liberty NangateOpenCellLibrary_typical_only_NAND2_X3.lib
+
+# Lower logic to basic internal primitives
+hierarchy -top full_adder
+proc; opt; techmap; opt
+
+# Force ABC to synthesize logic strictly into 2-input NAND primitives ($_NAND_$)
+abc -g NAND
+
+# Map internal generic $_NAND_$ primitives directly to NAND2_X3 instantiations
+techmap -map nand_map.v
+
+# Remove unused wires and format instance names cleanly
+clean
+rename -enumerate -pattern cell_%d t:NAND2_X3
+
 write_verilog -noattr fa_nand_mapped.v
